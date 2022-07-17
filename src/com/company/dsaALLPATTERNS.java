72a66da5c8d8fa18e97476f983f819be020f4267
Ansh <85667643+anshnain10@users.noreply.github.com>
2022-07-17 18:12:28 +0530

    Update dsaALLPATTERNS.java

--- src/com/company/dsaALLPATTERNS.java
+++ src/com/company/dsaALLPATTERNS.java
@@ -3,37 +3,28 @@ package com.company;
 import java.util.ArrayList;
 
 public class dsaALLPATTERNS {
-
-//print only one subsequence
-    static boolean PrintSubsSum2(int index, ArrayList<Integer> a, int[] arr, int n, int s, int sum) {
+    //count of subsequence whose sum=k
+    static int  PrintSubsSum2(int index, int[] arr, int n, int s, int sum) {
 
         if (index == n) {
             if (s == sum) {
-                for (int aa : a
-                ) {
-                    System.out.print(aa + " ");
-
-
-                }
-
-                System.out.println("");
-                return true;
+                return 1;
 
 
             }
-            else {return false;}
+            else {return 0;}
         } else {
-            a.add(arr[index]);
+
             s += arr[index];
-            if (PrintSubsSum2(index + 1, a, arr, n, s, sum) == true) {
-                return true;
-            }
+            int l=PrintSubsSum2(index + 1, arr, n, s, sum);
+
             s -= arr[index];
-            a.remove(a.size() - 1);
-            if (PrintSubsSum2(index + 1, a, arr, n, s, sum) == true) {
-                return true;
-            }
-            return false;
+
+           int r=PrintSubsSum2(index + 1,  arr, n, s, sum);
+
+
+        return l+r;
+
 
         }
 
@@ -42,15 +33,64 @@ public class dsaALLPATTERNS {
 
 
 
+
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 1};
         int n1 = arr1.length;
-        ArrayList<Integer> arrayList1 = new ArrayList<Integer>();
-        PrintSubsSum2(0, arrayList1, arr1, n1, 0, 3);
+
+        System.out.println(PrintSubsSum2(0, arr1, n1, 0, 3));
 
 
     }
 }
+
+//print only one subsequence
+//     static boolean PrintSubsSum2(int index, ArrayList<Integer> a, int[] arr, int n, int s, int sum) {
+
+//         if (index == n) {
+//             if (s == sum) {
+//                 for (int aa : a
+//                 ) {
+//                     System.out.print(aa + " ");
+
+
+//                 }
+
+//                 System.out.println("");
+//                 return true;
+
+
+//             }
+//             else {return false;}
+//         } else {
+//             a.add(arr[index]);
+//             s += arr[index];
+//             if (PrintSubsSum2(index + 1, a, arr, n, s, sum) == true) {
+//                 return true;
+//             }
+//             s -= arr[index];
+//             a.remove(a.size() - 1);
+//             if (PrintSubsSum2(index + 1, a, arr, n, s, sum) == true) {
+//                 return true;
+//             }
+//             return false;
+
+//         }
+
+
+//     }
+
+
+
+//     public static void main(String[] args) {
+//         int[] arr1 = {1, 2, 1};
+//         int n1 = arr1.length;
+//         ArrayList<Integer> arrayList1 = new ArrayList<Integer>();
+//         PrintSubsSum2(0, arrayList1, arr1, n1, 0, 3);
+
+
+//     }
+// }
 //static void PrintSubsSum2(int index, ArrayList<Integer> a, int[] arr, int n, int s, int sum) {
 //
 //        if (index == n) {
